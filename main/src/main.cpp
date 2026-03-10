16812e3932af8da26dda669ba6d06ee3ccc7b57b
wold9168 <moewold@outlook.com>
2026-03-10 21:52:22 +0800

    feat: implement the argument -V,--version
    Signed-off-by: wold9168 <moewold@outlook.com>

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -16,11 +16,23 @@ void argparse_initialize(argparse::ArgumentParser &program, const int &argc,
       .store_into(verbose)
       .nargs(0);
 
+  program.add_argument("-V", "--version")
+      .help("show version")
+      .default_value(false)
+      .implicit_value(true)
+      .nargs(0);
+
   program.add_argument("-h", "--help")
       .help("show help info")
       .default_value(false)
       .implicit_value(true)
       .nargs(0);
+
+  program.add_argument("-p", "--port")
+      .help("specify the port (default=8080)")
+      .default_value(8080)
+      .nargs(1)
+      .scan<'d', int>();
 
   try {
     program.parse_args(argc, argv);
@@ -39,6 +51,10 @@ int main(int argc, char **argv) {
   if (program["--help"] == true) {
     std::cout << program;
     exit(EXIT_SUCCESS);
+  } else if (program["--version"] == true) {
+    std::cout << APP_VERSION;
+    exit(EXIT_SUCCESS);
   }
+
   return 0;
 }
